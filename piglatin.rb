3d36816aee3f54ed53e1f84934ce49dd99fa633a
jaredphillips <jaredtphillips@me.com>
2013-06-10 14:16:56 -0400

    Adding Test Suite to Pig Latin Converter"

--- piglatin.rb
+++ piglatin.rb
@@ -1,11 +1,14 @@
 
 class PigLatinConverter
+
+
+
   def self.convert(word)
     word = word.downcase
     vowels = %w[a e i o u]
     consonant = []
     if vowels.include? word[0]
-      puts word.capitalize
+      return word.capitalize
     else
       word.each_char do |a|
         if vowels.include? a
@@ -14,19 +17,20 @@ class PigLatinConverter
           consonant.push a
         end
         x = consonant.length
-        puts word[x, 30].capitalize + consonant.join("").downcase + "ay"
+        end_of_word = word[x..-1].capitalize
+        first_consonant_set = consonant.join("").downcase
+        return end_of_word + first_consonant_set + "ay"
       end
     end
   end 
 end 
 
 
-PigLatinConverter.convert("apple")
-PigLatinConverter.convert("pop")
-PigLatinConverter.convert("Computer")
-PigLatinConverter.convert("Elephant")
-PigLatinConverter.convert("Quiet")
-PigLatinConverter.convert("Three")
+puts "What word do you want to convert?"
+@word = gets.chomp
+
+puts PigLatinConverter.convert("#{@word}")
+
 
 
 
